cb0ab5f47a6bae769895edaa47334ed988934996
jackwhelan <jackw97@live.ie>
2018-04-15 18:59:22 +0100

    Added player class

---
+++ Player.java
@@ -0,0 +1,35 @@
+package main;
+
+public class Player implements GameObject
+{
+	Rectangle playTangle;
+	int speed = 1;
+	
+	public Player()
+	{
+		playTangle = new Rectangle(32, 16, 16, 32);
+		playTangle.generateGraphics(2, 0xFF00FF90);
+	}
+	
+	// Call whenever possible
+	public void render(RenderHandler r, int xZoom, int yZoom)
+	{
+		r.renderRectangle(playTangle, xZoom, yZoom);
+	}
+	
+	// Call at 60 fps
+	public void update(Control game)
+	{
+		KeyBoardListener keyListener = game.getKeyListener();
+		
+		if(keyListener.up())
+			playTangle.y = playTangle.y - speed;
+		if(keyListener.down())
+			playTangle.y = playTangle.y + speed;
+		if(keyListener.left())
+			playTangle.x = playTangle.x - speed;
+		if(keyListener.right())
+			playTangle.x = playTangle.x + speed;
+		
+	}
+}
